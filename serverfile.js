a9afdd628c35a7cfa92c82a1b846033a8e595dad
Jiancong Xie <jimco@color7.net>
2013-12-02 15:16:48 +0800

    websocket openchat example

---
+++ serverfile.js
@@ -0,0 +1,20 @@
+var http = require('http')
+  , io = require('socket.io');  
+   
+var yourserver = http.createServer(function (request, response) {  
+    response.writeHead(250, { 'Content-Type': 'text/html' });  
+    response.end('Your WebSocket server is running');  
+}).listen(2222);
+   
+var yoursocket = io.listen(yourserver).set('log', 1);  
+   
+yoursocket.on('connection', function (client) {  
+    client.on('YourcustomMessage', function (data) {  
+      console.log('Client Custom Message: ', data);  
+      var current = new Date().getTime();  
+      client.broadcast.emit('YourMessageResponse', data + '(broadcasted)->' + current);  
+    });
+    client.on('disconnect', function () {  
+      console.log('Your Client disconnected');  
+    });  
+}); 
