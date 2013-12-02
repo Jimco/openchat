f69624c13de73c708c38c6b8775efacee1d46928
Jiancong Xie <jimco@color7.net>
2013-12-02 17:20:47 +0800

    chatroom beautify

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 var http = require('http')
   , io = require('socket.io');  
    
-var yourserver = http.createServer(function (request, response) {  
+var chatServer = http.createServer(function (request, response) {  
     response.writeHead(250, { 'Content-Type': 'text/html' });  
     response.end('Your WebSocket server is running');  
 }).listen(2222);
    
-var yoursocket = io.listen(yourserver).set('log', 1);  
+var chatSocket = io.listen(chatServer).set('log', 1);  
    
-yoursocket.on('connection', function (client) {  
+chatSocket.on('connection', function (client) {
     client.on('YourcustomMessage', function (data) {  
       console.log('Client Custom Message: ', data);  
       var current = new Date().getTime();  
