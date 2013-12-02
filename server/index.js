b35c6592c1a5ffdce8675df41de1bd65b8cfd5be
Jiancong Xie <jimco@color7.net>
2013-12-02 17:48:28 +0800

    modify style and add clear record fn

--- server/index.js
+++ server/index.js
@@ -2,19 +2,22 @@ var http = require('http')
   , io = require('socket.io');  
    
 var chatServer = http.createServer(function (request, response) {  
-    response.writeHead(250, { 'Content-Type': 'text/html' });  
-    response.end('Your WebSocket server is running');  
-}).listen(2222);
+      response.writeHead(250, { 'Content-Type': 'text/html' });  
+      response.end('WebSocket server is running');  
+  }).listen(2222);
    
 var chatSocket = io.listen(chatServer).set('log', 1);  
    
 chatSocket.on('connection', function (client) {
-    client.on('YourcustomMessage', function (data) {  
-      console.log('Client Custom Message: ', data);  
-      var current = new Date().getTime();  
-      client.broadcast.emit('YourMessageResponse', data + '(broadcasted)->' + current);  
-    });
-    client.on('disconnect', function () {  
-      console.log('Your Client disconnected');  
-    });  
+
+  client.on('chatMessage', function (data) {  
+    console.log('Client Custom Message: ', data);  
+    var current = new Date().getTime();
+    client.broadcast.emit('chatResponse', data + ' (Date: '+ current +')');
+  });
+
+  client.on('disconnect', function(){
+    console.log('Client disconnected');
+  });
+
 }); 
